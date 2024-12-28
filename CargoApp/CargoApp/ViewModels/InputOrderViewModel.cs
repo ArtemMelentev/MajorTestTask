20f5eeb151804ff7a574c57f2e13ef7b43a79bae
Artem Melentev <artem.melentev2@yandex.ru>
2024-12-28 17:24:07 +0700

    added order model in input order vm

--- CargoApp/CargoApp/ViewModels/InputOrderViewModel.cs
+++ CargoApp/CargoApp/ViewModels/InputOrderViewModel.cs
@@ -6,20 +6,126 @@ namespace CargoApp.ViewModels;
 
 public class InputOrderViewModel : InputViewModel
 {
-    private OrderStatus _orderStatus;
+    private string _clientName = String.Empty;
+    private string _courierName = String.Empty;
+    private string _cargoDetails = String.Empty;
+    private string _pickupAddress = String.Empty;
+    private string _deliveryAddress = String.Empty;
+    private string _comment = String.Empty;
+    private DateTime _creationDate = DateTime.Now;
+    private OrderStatus _orderStatus = OrderStatus.New;
     
     public ObservableCollection<OrderStatus> OrderStatuses { get; }
 
+    public string ClientName
+    {
+        get => _clientName;
+        set
+        {
+            if (value == _clientName)
+            {
+                return;
+            }
+            _clientName = value;
+            RaisePropertyChanged(nameof(ClientName));
+        }
+    }
+
+    public string CourierName
+    {
+        get => _courierName;
+        set
+        {
+            if (value == _courierName)
+            {
+                return;
+            }
+            _courierName = value;
+            RaisePropertyChanged(nameof(CourierName));
+        }
+    }
+
+    public string CargoDetails
+    {
+        get => _cargoDetails;
+        set
+        {
+            if (value == _cargoDetails)
+            {
+                return;
+            }
+            _cargoDetails = value;
+            RaisePropertyChanged(nameof(CargoDetails));
+        }
+    }
+
+    public string PickupAddress
+    {
+        get => _pickupAddress;
+        set
+        {
+            if (value == _pickupAddress)
+            {
+                return;
+            }
+            _pickupAddress = value;
+            RaisePropertyChanged(nameof(PickupAddress));
+        }
+    }
+
+    public string DeliveryAddress
+    {
+        get => _deliveryAddress;
+        set
+        {
+            if (value == _deliveryAddress)
+            {
+                return;
+            }
+            _deliveryAddress = value;
+            RaisePropertyChanged(nameof(DeliveryAddress));
+        }
+    }
+
+    public string Comment
+    {
+        get => _comment;
+        set
+        {
+            if (value == _comment)
+            {
+                return;
+            }
+            _comment = value;
+            RaisePropertyChanged(nameof(Comment));
+        }
+    }
+
+    public DateTime CreationDate
+    {
+        get => _creationDate;
+        set
+        {
+            if (value.Equals(_creationDate))
+            {
+                return;
+            }
+            _creationDate = value;
+            RaisePropertyChanged(nameof(CreationDate));
+        }
+    }
+    
     public OrderStatus SelectedStatus
     {
         get => _orderStatus;
         set
         {
-            if (_orderStatus != value)
+            if (_orderStatus == value)
             {
-                _orderStatus = value;
-                RaisePropertyChanged(nameof(SelectedStatus));
+                return;
             }
+            _orderStatus = value;
+            RaisePropertyChanged(nameof(SelectedStatus));
         }
     }
     
