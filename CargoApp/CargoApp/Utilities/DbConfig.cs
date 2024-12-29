68b1adf978d59d53e574c9404088f20a51a87229
Artem Melentev <artem.melentev2@yandex.ru>
2024-12-29 16:42:07 +0700

    refactoring

--- CargoApp/CargoApp/Utilities/DbConfig.cs
+++ CargoApp/CargoApp/Utilities/DbConfig.cs
@@ -2,9 +2,9 @@
 
 public class DbConfig
 {
-    public string Host { get; set; }
-    public string Port { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string DatabaseName { get; set; }
+    public string Host { get; set; } = String.Empty;
+    public string Port { get; set; } = String.Empty;
+    public string Username { get; set; } = String.Empty;
+    public string Password { get; set; } = String.Empty;
+    public string DatabaseName { get; set; } = String.Empty;
 }
